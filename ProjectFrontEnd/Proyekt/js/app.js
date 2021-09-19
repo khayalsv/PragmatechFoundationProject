60101c75453c6913cef930704bf80808f7f6fa86
Khayal Salimov <88320671+xeyalselimov@users.noreply.github.com>
2021-09-19 20:43:07 +0400

    alqoritm

--- ProjectFrontEnd/Proyekt/js/app.js
+++
@@ -1,7 +0,0 @@
-let cursor = document.querySelector('.cursor')
-let cursor2 = document.querySelector('.cursor2')
-document.addEventListener("mousemove", function(el) {
-    cursor.style.left = cursor2.style.left = el.pageX + 'px'
-    cursor.style.top = cursor2.style.top = el.pageY + 'px'
-
-})
